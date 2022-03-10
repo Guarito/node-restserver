b0572b718009a4d52b38dedf160d68f05c972bd0
Capassoft <capassomiguel@gmail.com>
2022-03-10 09:49:16 -0400

    13. Creado schema y mnodelo de Usuario en models/user

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "El nombre es obligatorio"],
+    },
+    email: {
+        type: String,
+        required: [true, "El correo es obligatorio"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "La contrasena es obligatoria"],
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    state: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = model("User", UserSchema); 
