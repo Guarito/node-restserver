da8d0e287b902320eadbb9c1fee93d738be39ef1
Capassoft <capassomiguel@gmail.com>
2022-03-12 19:17:36 -0400

    Validando roles desde DB. Creado models/role y documentos en mongo

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,8 @@ const { check } = require("express-validator");
 
 const { validateFields } = require("../middlewares/validate-fields");
 
+const Role = require("../models/role");
+
 const {
     usersGet,
     usersPost,
@@ -26,10 +28,18 @@ router.post(
     check("name", "Ingrese un nombre valido").notEmpty(),
     check("email", "Ingrese un correo valido.").isEmail(),
     check("password", "Debe contener mas de 6 digitos.").isLength({ min: 6 }),
-    check("role", "El rol definido no es valido.").isIn([
-        "ADMIN_ROLE",
-        "USER_ROLE",
-    ]),
+    // check("role", "El rol definido no es valido.").isIn([
+    //     "ADMIN_ROLE",
+    //     "USER_ROLE",
+    // ])
+    check("role").custom(async (role = "") => {
+        const existRole = await Role.findOne({ role: role });
+        if (!existRole) {
+            throw new Error(
+                `El rol ${role} no se encuentra validado en la base de datos.`
+            );
+        }
+    }),
     validateFields,
     usersPost
 );
